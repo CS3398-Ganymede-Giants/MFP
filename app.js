c867fa0b9e8dba2b60118761cb10c2c105fa8726
James <jolaroux@gmail.com>
2018-09-20 23:03:56 -0500

    t
    t

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var http = require('http');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-console.log("TEST")
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8080);
+
+console.log('Server started');
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// 
+// app.get('/', (req, res) => res.send('Hello World!'))
+// console.log("TEST")
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
