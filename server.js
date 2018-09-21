023fae2a4d1b1e4953493edf94e0014f92a4deb5
James <jolaroux@gmail.com>
2018-09-20 23:34:49 -0500

    t
    t

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080; 
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -11,10 +11,3 @@ http.createServer(function (request, response) {
 
 console.log('Server started');
 
-// const express = require('express')
-// const app = express()
-// const port = 3000
-// 
-// app.get('/', (req, res) => res.send('Hello World!'))
-// console.log("TEST")
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
