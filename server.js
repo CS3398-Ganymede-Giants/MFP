52ce6dee3ad1f518d0616001e30b0899ddf9768a
James <jolaroux@gmail.com>
2018-09-20 23:59:32 -0500

    t
    t

---
+++ server.js
@@ -0,0 +1,17 @@
+// Importing the http module
+var http = require('http');
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+// this is necessary because at deploy time, Heroku decides what port the application runs on.
+var port = process.env.PORT || 8080; 
+
+// Creating the server, telling it to listen on the port Heroku Assigns
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World!!!\nTesting Different Branches Deploying to Heroku');
+}).listen(port);
+
+//Simple console log confirmation
+console.log('Server started');
+
