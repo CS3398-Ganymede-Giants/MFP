287538da88612f2a1007634e6ba24e14265e47e5
James <jolaroux@gmail.com>
2018-09-20 23:21:36 -0500

    t
    t

--- server.js
+++ server.js
@@ -1,39 +1,20 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(port);
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+console.log('Server started');
 
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
-// var http = require('http');
+// const express = require('express')
+// const app = express()
+// const port = 3000
 // 
-// http.createServer(function (request, response) {
-//     response.writeHead(200, {'Content-Type': 'text/plain'});
-//     response.end('Hello World\n');
-// }).listen(8080);
-// 
-// console.log('Server started');
-// 
-// // const express = require('express')
-// // const app = express()
-// // const port = 3000
-// // 
-// // app.get('/', (req, res) => res.send('Hello World!'))
-// // console.log("TEST")
-// // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.get('/', (req, res) => res.send('Hello World!'))
+// console.log("TEST")
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
