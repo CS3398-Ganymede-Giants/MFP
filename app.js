cfd745c9a98a0ae0d14e6c7b1938f701cb48f0a5
James <jolaroux@gmail.com>
2018-11-05 20:14:18 -0600

    not closing client

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ herokuClient.query('SELECT table_schema,table_name FROM information_schema.table
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  herokuClient.end();
+//   herokuClient.end();
 });
 //end heroku code
 
