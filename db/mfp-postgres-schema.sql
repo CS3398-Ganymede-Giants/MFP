3179693d154b3df8529fb923e6140bece7db68fa
Robert Tomlinson <270105+robtomlinson@users.noreply.github.com>
2018-12-04 17:52:26 -0600

    Updated database schema SQL. Updated database schema UML diagram. Added text file of current/previous SQL query tests for reference.

--- db/mfp-postgres-schema.sql
+++ db/mfp-postgres-schema.sql
@@ -1,12 +1,24 @@
+-- Drop table
+
+-- DROP TABLE public.user_tbl
+
 CREATE TABLE user_tbl (
-	username varchar(40) NOT NULL UNIQUE,
+	username varchar(40) NOT NULL,
 	user_id serial NOT NULL,
 	passw text NOT NULL,
-  email_address varchar(255) NOT NULL UNIQUE,
 	firstname varchar(40) NULL,
 	lastname varchar(40) NULL,
+	email_address varchar(255) NOT NULL,
+	email_verified bool NULL DEFAULT false,
+	CONSTRAINT user_tbl_email_address_key UNIQUE (email_address),
 	CONSTRAINT user_tbl_pk PRIMARY KEY (user_id)
 );
+CREATE UNIQUE INDEX user_tbl_username_idx ON public.user_tbl USING btree (username);
+
+
+-- Drop table
+
+-- DROP TABLE public.expense_types_tbl
 
 CREATE TABLE expense_types_tbl (
 	expense_type_id serial NOT NULL,
@@ -14,16 +26,44 @@ CREATE TABLE expense_types_tbl (
 	CONSTRAINT expense_types_tbl_pkey PRIMARY KEY (expense_type_id)
 );
 
+
+-- Drop table
+
+-- DROP TABLE public.individual_expense_tbl
+
 CREATE TABLE individual_expense_tbl (
 	expense_id serial NOT NULL,
 	expense_type_id serial NOT NULL,
 	user_id serial NOT NULL,
 	description text NOT NULL,
 	cost_amount numeric NOT NULL DEFAULT 0.0,
+	"timestamp" timestamp NOT NULL DEFAULT now(),
+	account_type text NULL,
 	CONSTRAINT individual_expense_tbl_pk PRIMARY KEY (expense_id),
 	CONSTRAINT individual_expense_tbl_expense_types_tbl_fk FOREIGN KEY (expense_type_id) REFERENCES expense_types_tbl(expense_type_id) ON UPDATE CASCADE,
 	CONSTRAINT individual_expense_tbl_user_tbl_fk FOREIGN KEY (user_id) REFERENCES user_tbl(user_id) ON UPDATE CASCADE
 );
+
+-- Drop table
+
+-- DROP TABLE public.individual_income_tbl
+
+CREATE TABLE individual_income_tbl (
+	income_id serial NOT NULL,
+	account_id int4 NOT NULL,
+	description text NOT NULL,
+	income_amount numeric NOT NULL DEFAULT 0.0,
+	"timestamp" timestamp NOT NULL DEFAULT now(),
+	user_id numeric NULL,
+	account_type text NULL,
+	CONSTRAINT individual_income_tbl_pk PRIMARY KEY (income_id),
+	CONSTRAINT individual_income_tbl_account_tbl_fk FOREIGN KEY (account_id) REFERENCES account_tbl(account_id) ON UPDATE CASCADE
+);
+
+
+-- Drop table
+
+-- DROP TABLE public.expense_budget_tbl
 
 CREATE TABLE expense_budget_tbl (
 	user_id serial NOT NULL,
@@ -34,6 +74,11 @@ CREATE TABLE expense_budget_tbl (
 	CONSTRAINT expense_budget_tbl_user_tbl_fk FOREIGN KEY (user_id) REFERENCES user_tbl(user_id) ON UPDATE CASCADE
 );
 
+
+-- Drop table
+
+-- DROP TABLE public.account_tbl
+
 CREATE TABLE account_tbl (
 	account_id serial NOT NULL,
 	user_id serial NOT NULL,
@@ -42,5 +87,19 @@ CREATE TABLE account_tbl (
 	balance_goal numeric NOT NULL DEFAULT 0.0,
 	monthly_payment numeric NOT NULL DEFAULT 0.0,
 	CONSTRAINT account_tbl_pk PRIMARY KEY (account_id),
+	CONSTRAINT unique_user_account UNIQUE (user_id, account_type),
 	CONSTRAINT account_tbl_user_tbl_fk FOREIGN KEY (user_id) REFERENCES user_tbl(user_id) ON UPDATE CASCADE
+);
+
+-- Drop table
+
+-- DROP TABLE public.user_posts_tbl
+
+CREATE TABLE user_posts_tbl (
+	user_id int4 NOT NULL,
+	post_text text NULL,
+	"timestamp" timestamp NOT NULL DEFAULT now(),
+	user_posts_id serial NOT NULL,
+	CONSTRAINT user_posts_tbl_pk PRIMARY KEY (user_posts_id),
+	CONSTRAINT user_posts_tbl_user_tbl_fk FOREIGN KEY (user_id) REFERENCES user_tbl(user_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
